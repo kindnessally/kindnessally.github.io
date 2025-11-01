cbb7e479bd02a07508d08f3ad0caa5cc04220887
Jiaqi Guo <jiaqi.guo@gmail.com>
2025-11-01 12:41:46 -0500

    Add donate page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,8 @@
 import { Routes } from '@angular/router';
 import { Home } from '../home/home';
+import { Donate } from '../donate/donate';
 
 export const routes: Routes = [
-  {path: "", component: Home}
+  {path: "", component: Home},
+  {path: "donate", component: Donate},
 ];
