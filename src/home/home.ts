1a5c91ddd74e4e9faa3257605f9c28b91db373d8
Jiaqi Guo <jiaqi.guo@gmail.com>
2025-11-01 16:13:30 -0500

    Improve donation page

--- src/home/home.ts
+++ src/home/home.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
+import { MatIconModule } from '@angular/material/icon';
+import { RouterLink } from "@angular/router";
 
 @Component({
   selector: 'app-home',
-  imports: [ MatCardModule ],
+  imports: [MatCardModule, MatButtonModule, MatIconModule, RouterLink],
   templateUrl: './home.html',
   styleUrl: './home.scss',
 })
